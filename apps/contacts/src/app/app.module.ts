27b50069a0514c9040a7d3e6564f7bd6f68016e3
turlingxian <qq476424983@126.com>
2023-11-04 15:15:43 +0800

    1.0: file structure

--- apps/contacts/src/app/app.module.ts
+++ apps/contacts/src/app/app.module.ts
@@ -1,13 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreDataModule } from '@givaudan-production-angular/core-data';
+import { CoreStateModule } from '@givaudan-production-angular/core-state';
+import { MaterialModule } from '@givaudan-production-angular/material';
+import { AppComponent } from './app.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { RoutingModule } from './routing.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    ContactDetailComponent,
+    ContactsComponent,
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    CoreDataModule,
+    CoreStateModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    RoutingModule,
+    StoreModule.forRoot({}, {}),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,12 @@
 export interface Message {
   message: string;
 }
+
+export interface BaseEntity {
+  id: string | null;
+}
+
+export interface Widget extends BaseEntity {
+  name: string;
+  address: string;
+}
